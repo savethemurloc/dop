3966f67ec7688c509397faff85bd32cdd2a22f49
savethemurloc <bygoga1996@gmail.com>
2021-05-26 11:21:37 +0700

    добавлены вопросы, логистика между ними

--- logisticLogic.js
+++ logisticLogic.js
@@ -29,14 +29,74 @@
         showNewObs();
       }
 
+      function onClickCreateObs() {
+        hideAll();
+        showQuests1();
+      }
+
+      function onClickNext1Button() {
+        hideAll();
+        showQuests2();
+      }
+
+      function onClickNext2Button() {
+        hideAll();
+        showQuests3();
+      }
+
+      function onClickNext3Button() {
+        hideAll();
+        showQuests4();
+      }
+
+      function onClickNext4Button() {
+        hideAll();
+        showQuests5();
+      }
+
+      function onClickNext5Button() {
+        hideAll();
+        showQuests6();
+      }
+
+      function onClickNext6Button() {
+        hideAll();
+        showQuests7();
+      }
+
+      function onClickNext7Button() {
+        hideAll();
+        showQuests8();
+      }
+
+      function onClickNext8Button() {
+        hideAll();
+        showQuests9();
+      }
+
+      function onClickNext9Button() {
+        hideAll();
+        showQuests10();
+      }
+
+      function onClickNext10Button() {
+        hideAll();
+        showQuests11();
+      }
+
+      function onClickNext11Button() {
+        hideAll();
+        showQuests12();
+      }
+
+      function onClickNext12Button() {
+        hideAll();
+        showObs();
+      }
+
       function showGlav() {
         document.getElementById('glavnaya').removeAttribute("class");
         document.getElementById('glavnaya').setAttribute("class", "rightMain");
-      }
-
-      function hideGlav() {
-        document.getElementById('glavnaya').removeAttribute("class");
-        document.getElementById('glavnaya').setAttribute("class", "rightNone");
       }
 
       function showObs() {
@@ -44,19 +104,9 @@
         document.getElementById('allObs').setAttribute("class", "rightMain");
       }
 
-      function hideObs() {
-        document.getElementById('allObs').removeAttribute("class");
-        document.getElementById('allObs').setAttribute("class", "rightNone");
-      }
-
       function showStats() {
         document.getElementById('allStats').removeAttribute("class");
         document.getElementById('allStats').setAttribute("class", "rightMain");
-      }
-
-      function hideStats() {
-        document.getElementById('allStats').removeAttribute("class");
-        document.getElementById('allStats').setAttribute("class", "rightNone");
       }
 
       function showSprav() {
@@ -64,19 +114,9 @@
         document.getElementById('allSprav').setAttribute("class", "rightMain");
       }
 
-      function hideSprav() {
-        document.getElementById('allSprav').removeAttribute("class");
-        document.getElementById('allSprav').setAttribute("class", "rightNone");
-      }
-
       function showSettings() {
         document.getElementById('allSettings').removeAttribute("class");
         document.getElementById('allSettings').setAttribute("class", "rightMain");
-      }
-
-      function hideSettings() {
-        document.getElementById('allSettings').removeAttribute("class");
-        document.getElementById('allSettings').setAttribute("class", "rightNone");
       }
 
       function showNewObs() {
@@ -84,16 +124,101 @@
         document.getElementById('newObs').setAttribute("class", "rightMain");
       }
 
-      function hideNewObs() {
-        document.getElementById('newObs').removeAttribute("class");
-        document.getElementById('newObs').setAttribute("class", "rightNone");
+      function showQuests1() {
+          document.getElementById('Quests1').removeAttribute("class");
+          document.getElementById('Quests1').setAttribute("class", "rightMain");
+      }
+
+      function showQuests2() {
+        document.getElementById('Quests2').removeAttribute("class");
+        document.getElementById('Quests2').setAttribute("class", "rightMain");
+      }
+
+      function showQuests3() {
+        document.getElementById('Quests3').removeAttribute("class");
+        document.getElementById('Quests3').setAttribute("class", "rightMain");
+      }
+
+      function showQuests4() {
+        document.getElementById('Quests4').removeAttribute("class");
+        document.getElementById('Quests4').setAttribute("class", "rightMain");
+      }
+
+      function showQuests5() {
+        document.getElementById('Quests5').removeAttribute("class");
+        document.getElementById('Quests5').setAttribute("class", "rightMain");
+      }
+
+      function showQuests6() {
+        document.getElementById('Quests6').removeAttribute("class");
+        document.getElementById('Quests6').setAttribute("class", "rightMain");
+      }
+
+      function showQuests7() {
+        document.getElementById('Quests7').removeAttribute("class");
+        document.getElementById('Quests7').setAttribute("class", "rightMain");
+      }
+
+      function showQuests8() {
+        document.getElementById('Quests8').removeAttribute("class");
+        document.getElementById('Quests8').setAttribute("class", "rightMain");
+      }
+
+      function showQuests9() {
+        document.getElementById('Quests9').removeAttribute("class");
+        document.getElementById('Quests9').setAttribute("class", "rightMain");
+      }
+
+      function showQuests10() {
+        document.getElementById('Quests10').removeAttribute("class");
+        document.getElementById('Quests10').setAttribute("class", "rightMain");
+      }
+
+      function showQuests11() {
+        document.getElementById('Quests11').removeAttribute("class");
+        document.getElementById('Quests11').setAttribute("class", "rightMain");
+      }
+
+      function showQuests12() {
+        document.getElementById('Quests12').removeAttribute("class");
+        document.getElementById('Quests12').setAttribute("class", "rightMain");
       }
 
       function hideAll() {
-        hideObs();
-        hideGlav();
-        hideSprav();
-        hideStats();
-        hideSettings();
-        hideNewObs();
+        document.getElementById('allObs').removeAttribute("class");
+        document.getElementById('allObs').setAttribute("class", "rightNone");
+        document.getElementById('glavnaya').removeAttribute("class");
+        document.getElementById('glavnaya').setAttribute("class", "rightNone");
+        document.getElementById('allSprav').removeAttribute("class");
+        document.getElementById('allSprav').setAttribute("class", "rightNone");
+        document.getElementById('allStats').removeAttribute("class");
+        document.getElementById('allStats').setAttribute("class", "rightNone");
+        document.getElementById('allSettings').removeAttribute("class");
+        document.getElementById('allSettings').setAttribute("class", "rightNone");
+        document.getElementById('newObs').removeAttribute("class");
+        document.getElementById('newObs').setAttribute("class", "rightNone");
+        document.getElementById('Quests1').removeAttribute("class");
+        document.getElementById('Quests1').setAttribute("class", "rightNone");
+        document.getElementById('Quests2').removeAttribute("class");
+        document.getElementById('Quests2').setAttribute("class", "rightNone");
+        document.getElementById('Quests3').removeAttribute("class");
+        document.getElementById('Quests3').setAttribute("class", "rightNone");
+        document.getElementById('Quests4').removeAttribute("class");
+        document.getElementById('Quests4').setAttribute("class", "rightNone");
+        document.getElementById('Quests5').removeAttribute("class");
+        document.getElementById('Quests5').setAttribute("class", "rightNone");
+        document.getElementById('Quests6').removeAttribute("class");
+        document.getElementById('Quests6').setAttribute("class", "rightNone");
+        document.getElementById('Quests7').removeAttribute("class");
+        document.getElementById('Quests7').setAttribute("class", "rightNone");
+        document.getElementById('Quests8').removeAttribute("class");
+        document.getElementById('Quests8').setAttribute("class", "rightNone");
+        document.getElementById('Quests9').removeAttribute("class");
+        document.getElementById('Quests9').setAttribute("class", "rightNone");
+        document.getElementById('Quests10').removeAttribute("class");
+        document.getElementById('Quests10').setAttribute("class", "rightNone");
+        document.getElementById('Quests11').removeAttribute("class");
+        document.getElementById('Quests11').setAttribute("class", "rightNone");
+        document.getElementById('Quests12').removeAttribute("class");
+        document.getElementById('Quests12').setAttribute("class", "rightNone");
       }
